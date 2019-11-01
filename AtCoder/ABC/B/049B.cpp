fc3d5efb5ba72f72d6bc63fb9842c204d95cd983
ValentinaChaconBuitrago <v.chacon@uniandes.edu.co>
2019-11-01 13:42:28 -0500

    B

---
+++ AtCoder/ABC/B/049B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int h,w;
+	cin >> h >> w;
+	while(h--){
+		string c;
+		cin >> c;
+		cout << c << endl << c << endl;
+	}
+	return 0;
+}

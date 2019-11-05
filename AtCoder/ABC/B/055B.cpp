ea559b7b2148b431234fcecdb273673591a16919
ValentinaChaconBuitrago <v.chacon@uniandes.edu.co>
2019-11-05 18:28:19 -0500

    B

---
+++ AtCoder/ABC/B/055B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define mods 1000000000+7;
+typedef long long ll; 
+
+int main(){
+
+	int n;
+	cin >> n;
+	ll power = 1;
+	for(int i=1; i<=n;++i){
+		power*=i;
+		power%=mods;
+	}
+	cout << power << endl;
+	return 0;
+}
